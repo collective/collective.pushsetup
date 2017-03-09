90b6730ebc8d5bd9b085412364b7f8d0f5748712
Bernhard Pöttinger <bernhard.poettinger@tngtech.com>
2017-03-09 10:19:28 +0100

    overhaul error handling, add logger

--- src/collective/pushsetup/pushsetup.py
+++ src/collective/pushsetup/pushsetup.py
@@ -1,7 +1,10 @@
 import argparse
 import tarfile
+import sys
+import logging
 import git
 import StringIO
+
 
 from AccessControl.SecurityManagement import newSecurityManager
 from Testing import makerequest
@@ -28,6 +31,21 @@ def getSite(app, site_id, admin_id):
     return site
 
 
+def getLogger(logfilename='var/log/pushsetup.log'):
+    logger = logging.getLogger('pushsetup')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(logfilename)
+    fh.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(file_formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+
+    return logger
+
+
 def makeArgParser():
     parser = argparse.ArgumentParser(description='Acquire Plone site\'s Generic Setup state and push it '
                                                  'to git repository via local proxy repository.'
@@ -47,36 +65,58 @@ def makeArgParser():
 
 
 def main(app):
+    logger = getLogger()
+
+    parser = makeArgParser()
+    args = parser.parse_args()
+
+    site = getSite(app, args.site, args.user)
+    stool = site.portal_setup
+
+    logger.info("Acquiring Generic Setup state from site '%s' using user '%s'", args.site, args.user)
+
+    result = stool.runAllExportSteps()
+    tarball = StringIO.StringIO(result['tarball'])
+
     try:
-        parser = makeArgParser()
-        args = parser.parse_args()
-
-        site = getSite(app, args.site, args.user)
-        stool = site.portal_setup
-
-        result = stool.runAllExportSteps()
-        tarball = StringIO.StringIO(result['tarball'])
-
         # access repository (master branch)
         repository = git.Repo(args.repo)
         repository.git.checkout('master')
+        remote = repository.remote('origin')
+    except (git.InvalidGitRepositoryError, git.NoSuchPathError, git.GitCommandError, ValueError) as e:
+        logger.exception('Failed accessing local git repository')
+        sys.exit(1)
 
+    logger.info("Extracting state into local repository '%s'", args.repo)
+
+    try:
         # extract dump into repository
         with tarfile.open(fileobj=tarball, errorlevel=2) as archive:
             archive.extractall(args.repo)
+    except tarfile.TarError as e:
+        logger.exception('Failed extracting state to local repository')
+        sys.exit(1)
 
+    logger.info("Commiting changes to local repository '%s'", args.repo)
+
+    try:
         # commit all changes to local repository
         repository.git.add('--all')
         repository.git.commit('--allow-empty', '-m', 'update config dump')
+    except git.GitCommandError as e:
+        logger.exception('Failed commiting changes to local repository')
+        sys.exit(1)
 
-        # push commit to remote repository
-        repository.remotes['origin'].push()
+    logger.info("Pushing changes to remote repository '%s'", remote.name)
 
-    except Exception:
-        print "Failed to push Plone state to repository"
-        raise
+    try:
+        # push commit from local to remote repository
+        remote.push()
+    except git.GitCommandError as e:
+        logger.exception('Failed pushing changes to remote repository')
+        sys.exit(1)
 
-    print "Pushed Plone state to repository"
+    logger.info("Pushed Plone state to remote repository")
 
 
 if "app" in locals():
